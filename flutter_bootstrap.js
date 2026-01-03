9361697339c2ff30823443c45e5d798256c681b5
Henrik Zaenger <henrik@zaenger.lu>
2026-01-03 21:03:28 +0100

    hotfix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1470407463"
+    serviceWorkerVersion: "3038137909"
   }
 });
